b15cb1faadbcd525c99a06076d5e2160c3fa90c9
noahhelton98 <noahhelton98@gmail.com>
2021-03-29 20:41:20 -0400

    added all initial HTML and JS to make a functioning application

---
+++ assets/js/script.js
@@ -0,0 +1,54 @@
+//get time variables 
+var date = $('#currentDay');
+var currentHour = moment().format('H');
+
+
+//Make arrays for inputs and military times
+var inputAreas = [];
+var militaryTimes = [];
+for (var i = 9; i < 18; i++){
+    inputAreas.push($(`#${i} textarea`))
+    militaryTimes.push(i);
+
+}
+
+//WHEN I refresh the page
+//THEN the saved events persist
+for (var i = 9; i < 18; i++){
+    var retrievedNote = JSON.parse(localStorage.getItem(`${i}`));
+    console.log(retrievedNote)
+    if (retrievedNote.value == ''){
+        inputAreas[i-9][0].innerHTML = retrievedNote;
+    }else{
+        inputAreas[i-9][0].innerHTML =  retrievedNote;
+}
+}
+
+//Set the colors based on the current time
+for (var i = 0; i < inputAreas.length; i++){
+    if (militaryTimes[i] < currentHour){
+        inputAreas[i].addClass('past');
+    }else if (militaryTimes[i] > currentHour){
+        inputAreas[i].addClass('future')
+    }else{
+        inputAreas[i].addClass('present')
+    }
+}
+//On the save button save whatever text is in local storage
+$('button').on('click', function (event){
+    event.preventDefault();
+    var slot = $(this).parent().attr('id');
+    var note = $(this).prev().val(); 
+    localStorage.setItem(slot, JSON.stringify(note))
+})
+
+//clock function 
+//THEN the current day is displayed at the top of the calendar
+let startClock = function(){
+    var today = moment();
+    date.text(today.format("MMM Do, YYYY h:mm:ss a"));
+}
+
+//get time to be dynamic
+setInterval(startClock, 1000);
+startClock();
